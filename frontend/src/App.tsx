84f593596195f890c5c89a36a8351e3242cb6fab
Siam <siam@evatix.com>
2022-04-22 20:24:35 +0600

    update

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ color: "black", fontSize: "2rem", marginTop: "2rem" }}>
+      Parcel
+    </div>
+  );
+}
